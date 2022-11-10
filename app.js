8cbb3ec6424ea6a7f59cd8b1906e5eb32d6514ea
Rasul <rasulakhmadullin0@gmail.com>
2022-11-10 10:37:49 +0300

    just in case

---
+++ app.js
@@ -0,0 +1,10 @@
+const myName = 'Rasul'
+const mySurname = 'Akhmadullin'
+let myFavoriteDrink = 'black Tea'
+const myFavoriteAnimal = 'cat'
+const myFavoriteProgrammingLanguage = 'JavaScript'
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
